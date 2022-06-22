7e2966d56a4a47100f6a0664d3813f2d882cad27
Lizzy <rhodeslizzy19@gmail.com>
2022-06-22 13:47:18 -0500

    added datasets

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ function timesClickedHandler(card){
     console.log(randomNumberOne, randomNumberTwo, randomNumberThree)
 
 
-    
+
   // I WANT TO RANDOMIZE THEIR POSITION
   // we need a for loop to iterate over the cardNames array
   for ( let i = 0; i< cardNames.length; i++){
@@ -191,8 +191,32 @@ resultsButton.addEventListener('click', function(){
                 'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
-        }]
+        },
+        {
+          label: '# of views',
+          data: productViews, // Has to match the label data!!
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      },
+      ]
     },
+
+    
     options: {
         scales: {
             y: {
